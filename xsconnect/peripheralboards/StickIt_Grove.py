c7934eb5d7ecded9f0f0de95c7d1a877c6a0060e
Dave Vandenbout <devb@xess.com>
2015-07-14 16:08:09 -0400

    Added definition file for the StickIt! Grove board.

---
+++ xsconnect/peripheralboards/StickIt_Grove.py
@@ -0,0 +1,17 @@
+brd = {
+    'name': ('StickIt! Grove V1.0'),
+    'port': {
+        'pmod': {
+            'default' : {
+                'gr1-d0': 'd0',
+                'gr1-d1': 'd1',
+                'gr2-d2': 'd2',
+                'gr2-d3': 'd3',
+                'gr4-d4': 'd4',
+                'gr4-d5': 'd5',
+                'gr3-d6': 'd6',
+                'gr3-d7': 'd7'
+            }
+        }
+    }
+}
