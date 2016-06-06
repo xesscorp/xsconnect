9b5df029b3ec566e5af0ed54bde8bc9817fb202e
Dave Vandenbout <devb@xess.com>
2016-06-06 19:09:09 -0400

    Added I/O assignments for the StickIt! RotaryEncoder V2.0 peripheral board.

---
+++ xsconnect/peripheralboards/StickIt_Rotary_Encoder_V2.py
@@ -0,0 +1,15 @@
+brd = {
+    'name': ('StickIt! Rotary Encoder V2'),
+    'port': {
+        'pmod': {
+            'default': {
+                'btn1': 'd2',
+                'a1': 'd0',
+                'b1': 'd1',
+                'btn2': 'd4',
+                'a2': 'd6',
+                'b2': 'd7',
+            }
+        },
+    }
+}
