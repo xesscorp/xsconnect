3ba44c31f9a396ebc048c15106d7c04e83cdf0b6
Dave Vandenbout <devb@xess.com>
2016-06-08 09:24:15 -0400

    Added I/O assignments for the StickIt! AudioIO V2.0 peripheral board. * Added I/O assignments for the StickIt! OctoADC V1.0 peripheral board.

---
+++ xsconnect/peripheralboards/StickIt_AudioIO_V2.py
@@ -0,0 +1,17 @@
+brd = {
+    'name': ('StickIt! Audio I/O V2'),
+    'port': {
+        'pmod': {
+            'default' : {
+                'sdto' : 'd0',
+                'sdti' : 'd1',
+                'lrck' : 'd2',
+                'mclk' : 'd3',
+                'cs_n' : 'd4',
+                'sclk' : 'd5',
+                'rst_n': 'd6',
+                'cclk' : 'd7',
+            }
+        }
+    }
+}
---
+++ xsconnect/peripheralboards/StickIt_OctoADC_V1.py
@@ -0,0 +1,13 @@
+brd = {
+    'name': ('StickIt! OctoADC V1'),
+    'port': {
+        'pmod': {
+            'default' : {
+                'dout' : 'd1',
+                'sclk' : 'd3',
+                'din'  : 'd5',
+                'cs_n' : 'd7',
+            }
+        }
+    }
+}
