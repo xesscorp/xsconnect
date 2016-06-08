cb2cf7a5d6e8ae7d1981e978f063c1f23bf1bb57
Dave Vandenbout <devb@xess.com>
2016-06-08 09:23:46 -0400

    Added I/O assignments for the StickIt! SmallButtons V1.0 peripheral board.

---
+++ xsconnect/peripheralboards/StickIt_SmallButtons_V1.py
@@ -0,0 +1,17 @@
+brd = {
+    'name': ('StickIt! SmallButtons V1'),
+    'port': {
+        'pmod': {
+            'default' : {
+                's1': 'd5',
+                's2': 'd4',
+                's3': 'd7',
+                's4': 'd6',
+                's5': 'd3',
+                's6': 'd2',
+                's7': 'd1',
+                's8': 'd0',
+            }
+        },
+    }
+}
