97b17c374ae8412683ff5f53973926b5e0fe4c52
Dave Vandenbout <devb@xess.com>
2016-06-09 23:37:34 -0400

    Added I/O assignments for the StickIt! QuadDAC V1.0 peripheral board.

---
+++ xsconnect/peripheralboards/StickIt_QuadDAC_V1.py
@@ -0,0 +1,17 @@
+brd = {
+    'name': ('StickIt! QuadDAC V1'),
+    'port': {
+        'pmod': {
+            'default' : {
+                'sdi3'   : 'd0',
+                'sdo3'   : 'd1',
+                'sdi1'   : 'd2',
+                'cs3_n'  : 'd3',
+                'ldac_n' : 'd4',
+                'cs12_n' : 'd5',
+                'sdi2'   : 'd6',
+                'sck'    : 'd7',
+            }
+        }
+    }
+}
